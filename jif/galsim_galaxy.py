bc928cd0915c2bd246096b9f615423a9f243dd18
Michael D. Schneider <mdschneider@me.com>
2015-10-22 00:03:59 -0700

    Update hierarchy in segment file definition

--- jif/galsim_galaxy.py
+++ jif/galsim_galaxy.py
@@ -473,30 +473,33 @@ def make_test_images(filter_name_ground='r', filter_name_space='y', file_lab='',
     seg = segments.Segments(segfile)
 
     dummy_mask = 1.0
+    dummy_background = 0.0
 
     ### Ground data
     seg.save_images([lsst_data], [lsst.noise.getVariance()], [dummy_mask],
-        group_name='ground', source_extraction='sextractor',
+        [dummy_background],
+        telescope='lsst',
         filter_name=filter_name_ground)
-    seg.save_tel_metadata(group_name='ground', telescope='LSST',
+    seg.save_tel_metadata(telescope='lsst',
         primary_diam=lsst.primary_diam_meters,
         pixel_scale_arcsec=lsst.pixel_scale,
         atmosphere=lsst.atmosphere)
     seg.save_psf_images([lsst.get_psf_image().array],
-        group_name='ground', source_extraction='sextractor',
+        telescope='lsst',
         filter_name=filter_name_ground)
     ### TODO: Save bandpass lookup tables
 
     ### Space data
     seg.save_images([wfirst_data], [wfirst.noise.getVariance()], [dummy_mask],
-        group_name='space', source_extraction='sextractor',
+        [dummy_background],
+        telescope='wfirst',
         filter_name=filter_name_space)
-    seg.save_tel_metadata(group_name='space', telescope='WFIRST',
+    seg.save_tel_metadata(telescope='wfirst',
         primary_diam=wfirst.primary_diam_meters,
         pixel_scale_arcsec=wfirst.pixel_scale,
         atmosphere=wfirst.atmosphere)
     seg.save_psf_images([wfirst.get_psf_image().array],
-        group_name='space', source_extraction='sextractor',
+        telescope='wfirst',
         filter_name=filter_name_space)
     ### TODO: Save bandpass lookup tables
 
--- jif/segments.py
+++ jif/segments.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Utilities for creating and parsing 'segment' files
+Utility for creating and parsing 'segment' files
 
 A segment file must include the following information:
 
@@ -35,10 +35,10 @@ class Segments(object):
 
         self.file = h5py.File(segment_file, 'w')
 
-    def save_tel_metadata(self, group_name='ground', telescope='lsst',
+    def save_tel_metadata(self, telescope='lsst',
                           primary_diam=8.4, pixel_scale_arcsec=0.2,
                           atmosphere=True):
-        g = create_group(self.file, group_name)
+        g = create_group(self.file, telescope)
         g.attrs['telescope'] = telescope
         g.attrs['primary_diam'] = primary_diam
         g.attrs['pixel_scale_arcsec'] = pixel_scale_arcsec
@@ -55,16 +55,15 @@ class Segments(object):
                     image_list,
                     noise_list,
                     mask_list,
-                    group_name = 'ground',
-                    source_extraction = 'sextractor',
+                    background_list,
+                    telescope = 'lsst',
                     filter_name = 'r'):
         """
         Save images for the segments from a single telescope
         """
-        segment_name = '{}/observation/{}/segments'.format(group_name,
-            source_extraction)
+        segment_name = 'segment/{}/band_{}'.format(telescope, filter_name)
         for iseg, im in enumerate(image_list):
-            seg = create_group(self.file, segment_name + '/{:d}'.format(iseg))
+            seg = create_group(self.file, segment_name + '/epoch_{:d}'.format(iseg))
             # image - background
             seg.create_dataset('image', data=im)
             # rms noise
@@ -72,30 +71,38 @@ class Segments(object):
             # estimate the variance of this noise image and save as attribute
             seg.attrs['variance'] = np.var(noise_list[iseg])
             seg.create_dataset('segmask', data=mask_list[iseg])
+            seg.create_dataset('background', data=background_list[iseg])
         return None
 
     def save_psf_images(self,
                         image_list,
-                        group_name='ground',
-                        source_extraction='sextractor',
-                        filter_name='r'):
+                        telescope='lsst',
+                        filter_name='r',
+                        model_names=None):
         """
         Save an image of the estimated PSF for each segment
+
+        The elements of 'image_list' do not have to be images. In this case,
+        specify how to parse the 'image' replacements with a list of descriptive
+        strings in 'model_names'.
         """
-        segment_name = '{}/observation/{}/segments'.format(group_name,
-            source_extraction)
+        segment_name = 'segment/{}/band_{}'.format(telescope, filter_name)
         for iseg, im in enumerate(image_list):
-            seg = create_group(self.file, segment_name + '/{:d}'.format(iseg))
+            seg = create_group(self.file, segment_name + '/epoch_{:d}'.format(iseg))
             seg.create_dataset('psf', data=im)
+            if model_names is None:
+                seg.attrs['psf_type'] = 'image'
+            else: ### Assume a list of names of PSF model types
+                seg.attrs['psf_type'] = model_names[iseg]
         return None
 
     def save_bandpasses(self, filters_list, waves_nm_list, throughputs_list,
-                        group_name='ground'):
+                        telescope='lsst'):
         """
         Save bandpasses for a single telescope as lookup tables.
         """
         for i, filter_name in enumerate(filters_list):
-            bp = self.file.create_group('{}/filters/{}'.format(group_name,
+            bp = self.file.create_group('{}/filters/{}'.format(telescope,
                 filter_name))
             bp.create_dataset('waves_nm', data=waves_nm_list[i])
             bp.create_dataset('throughput', data=throughputs_list[i])
