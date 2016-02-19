d94ec26e12a2ad57601f2dafb479c85a616e504f
Michael D. Schneider <schneider42@llnl.gov>
2016-02-19 10:09:55 -0800

    Update package imports

--- jif/__init__.py
+++ jif/__init__.py
@@ -1,3 +1,5 @@
 from segments import Segments
-from psf_model import PSFModel
+from psf_model import PSFModel, DefaultPriorPSF
 from galsim_galaxy import GalSimGalaxyModel, lsst_noise, wfirst_noise
+from Roaster import Roaster, DefaultPriorSpergel, DefaultPriorBulgeDisk
+from Roaster import do_sampling as do_roaster_sampling
