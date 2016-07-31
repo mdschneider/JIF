a7df1f35e053e36971732fd30feef3288c153e77
Michael D. Schneider <mdschneider@me.com>
2016-07-31 07:38:30 -0700

    Update SED tests to unittest and new(ish) GalSim API

--- tests/test_sed.py
+++ tests/test_sed.py
@@ -1,83 +1,84 @@
+import unittest
 import sys
 import os
 import numpy as np
 import galsim
 
+path, filename = os.path.split(__file__)
+datapath = os.path.abspath(os.path.join(path, "../jif/input/"))
+
 #
 # We depend on specific galsim.SED addition and magnitude operations
 # Test here that these perform as we need them to.
 #
+class TestSEDInstances(unittest.TestCase):
 
-path, filename = os.path.split(__file__)
-datapath = os.path.abspath(os.path.join(path, "../input/"))
+    def __init__(self, *args, **kwargs):
+        super(TestSEDInstances, self).__init__(*args, **kwargs)
+        self.redshift = 1.0
+        self.filter_name = 'r'
 
-redshift = 1.0
-filter_name = 'r'
+        filter_filename = os.path.join(datapath, 'LSST_{0}.dat'.format(self.filter_name))
+        bp = galsim.Bandpass(filter_filename, wave_type='nm')
+        self.bp = bp.withZeropoint('AB', effective_diameter=100. * 8.6, exptime=30)
 
-filter_filename = os.path.join(datapath, 'LSST_{0}.dat'.format(filter_name))
-bp = galsim.Bandpass(filter_filename)
-bp = bp.withZeropoint('AB', effective_diameter=100. * 8.6, exptime=30)
+    def test_SED_small_flux(self):
+        """
+        Test that assigning a large apparent magnitude yields a small output flux of an SED
+        """
+        mag = 99.
 
+        SED_name = "NGC_0695_spec"
+        SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
+        SED = galsim.SED(SED_filename, wave_type='Ang', flux_type='flambda')
 
-def test_SED_small_flux():
-    """
-    Test that assigning a large apparent magnitude yields a small output flux of an SED
-    """
-    mag = 99.
+        ### Set the magnitude at zero and evaluate outputs at a different redshift
+        SED = SED.atRedshift(0.).withMagnitude(target_magnitude=mag, bandpass=self.bp)
+        SED = SED.atRedshift(self.redshift)
 
-    SED_name = "NGC_0695_spec"
-    SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
-    SED = galsim.SED(SED_filename, wave_type='Ang')
+        flux = SED.calculateFlux(self.bp)
+        # print "flux: {:8.6e}".format(flux)
+        self.assertLess(flux, 1.e-20)
 
-    ### Set the magnitude at zero and evaluate outputs at a different redshift
-    SED = SED.atRedshift(0.).withMagnitude(target_magnitude=mag, bandpass=bp)
-    SED = SED.atRedshift(redshift)
+        mag_out = SED.calculateMagnitude(self.bp)
+        # print "mag: {:8.6f}".format(mag_out)
+        self.assertGreater(mag_out, 90.)
 
-    flux = SED.calculateFlux(bp)
-    # print "flux: {:8.6e}".format(flux)
-    assert flux < 1.e-20
+        return None
 
-    mag_out = SED.calculateMagnitude(bp)
-    # print "mag: {:8.6f}".format(mag_out)
-    assert mag_out > 90.
+    def test_SED_add(self):
+        """
+        Test that adding an SED with small amplitude does not change the output flux/magnitude
 
-    return None
+        This mimics the operations in galsim_galaxy.py::GalsimGalaxyModel.set_mag_from_obs
+        """
+        mag_a = 20.
+        mag_b = 99.
 
+        SED_name = "NGC_0695_spec"
+        SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
+        SED_a = galsim.SED(SED_filename, wave_type='Ang', flux_type='flambda')
+        SED_a = SED_a.atRedshift(0.).withMagnitude(target_magnitude=mag_a, bandpass=self.bp)
+        SED_a = SED_a.atRedshift(self.redshift)
 
-def test_SED_add():
-    """
-    Test that adding an SED with small amplitude does not change the output flux/magnitude
+        SED_name = "NGC_4125_spec"
+        SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
+        SED_b = galsim.SED(SED_filename, wave_type='Ang', flux_type='flambda')
+        SED_b = SED_b.atRedshift(0.).withMagnitude(target_magnitude=mag_b, bandpass=self.bp)
+        SED_b = SED_b.atRedshift(self.redshift)
 
-    This mimics the operations in galsim_galaxy.py::GalsimGalaxyModel.set_mag_from_obs
-    """
-    mag_a = 20.
-    mag_b = 99.
+        mag_add_out = (SED_a + SED_b).calculateMagnitude(self.bp)
 
-    SED_name = "NGC_0695_spec"
-    SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
-    SED_a = galsim.SED(SED_filename, wave_type='Ang')
-    SED_a = SED_a.atRedshift(0.).withMagnitude(target_magnitude=mag_a, bandpass=bp)
-    SED_a = SED_a.atRedshift(redshift)
+        mag_a_out = SED_a.calculateMagnitude(self.bp)
+        mag_b_out = SED_b.calculateMagnitude(self.bp)
 
-    SED_name = "NGC_4125_spec"
-    SED_filename = os.path.join(datapath, '{0}.sed'.format(SED_name))
-    SED_b = galsim.SED(SED_filename, wave_type='Ang')
-    SED_b = SED_b.atRedshift(0.).withMagnitude(target_magnitude=mag_b, bandpass=bp)
-    SED_b = SED_b.atRedshift(redshift)
+        # print "mag_a_out: {:8.6f}, mag_b_out: {:8.6f}, mag_add_out: {:8.6f}".format(
+        #     mag_a_out, mag_b_out, mag_add_out)
 
-    mag_add_out = (SED_a + SED_b).calculateMagnitude(bp)
+        self.assertAlmostEqual(mag_add_out, mag_a_out, 4)
 
-    mag_a_out = SED_a.calculateMagnitude(bp)
-    mag_b_out = SED_b.calculateMagnitude(bp)
-
-    # print "mag_a_out: {:8.6f}, mag_b_out: {:8.6f}, mag_add_out: {:8.6f}".format(
-    #     mag_a_out, mag_b_out, mag_add_out)
-
-    assert np.isclose(mag_add_out, mag_a_out)
-
-    return None
+        return None
 
 
 if __name__ == "__main__":
-    test_SED_small_flux()
-    test_SED_add()
+    unittest.main()
